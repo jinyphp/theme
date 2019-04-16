f3c98b5a3f314d7dbbcf1b83d9ea26517495f9f3
hojin <infohojin@gmail.com>
2019-04-16 23:30:44 +0900

    fix body access method

--- src/Theme.php
+++ src/Theme.php
@@ -20,17 +20,13 @@ class Theme
 
     use PreFix;
 
-    
-
     // 인스턴스
     private $App;
     public $View;
 
-    
     public $_env=[];
+    const FRONTMATTER = "page";
     
-
-
     /**
      * 인스턴스
      */
@@ -50,142 +46,6 @@ class Theme
             return self::$Instance; 
         }
     }
-
-
-    /**
-     * 테마 초기화
-     */
-    public function __construct($view=null)
-    {
-        // 객체참조 개선을 위해서 임시저장합니다.
-        // $this->View = Registry::get("View"); 
-        // $this->App = Registry::get("App"); 
-  
-
-        // 매소드 속도개선을 위해서
-        // 초기화 작업을 합니다.
-        //$this->_path = conf("ENV.path.theme");
-        $this->path();
-    }
-
-    public $path;
-    /**
-     * 테마 경로
-     */
-    public function path()
-    {
-        if(isset($this->path)) {
-            return $this->path;
-        } else {
-            $this->path = conf("ENV.path.theme");
-            return $this->path;
-        }
-    }
-
-    /**
-     * 테마 이를을 확인합니다.
-     */
-    public function name($body)
-    {
-        // 커스텀 테마 확인
-        if (isset($body->_data['page']['theme'])) {
-            // 머리말에 적용할 테마를 직접 지정을 할 경우
-            // 우선 처리합니다.
-            return $body->_data['page']['theme'];
-        
-        } else {
-            // 기본환경 설정을 적용합니다.
-            return conf("site.theme");
-        }  
-    }
-
-
-    /**
-     * 테마를 랜더링 합니다.
-     * 탬플릿 메소드 패턴으로 구현합니다.
-     */
-    public function render($html=null)
-    {
-        if ($this->_env['layout']) { 
-            $layout = \jiny\layout();
-            $code = $this->_env['_content'];
-        } else {
-            // 레이아웃이 없는 경우 바로 출력합니다.
-            $layout = (new Show($this))->progress();
-            $code = "{(content)}";
-        }
-
-        // 본문을 치환합니다.            
-        $layout = str_replace($code, $html->_body, $layout); 
-        $html->setBody($layout);
-    }
-
-
-    /**
-     * 테마의 설정을 읽어 옵니다.
-     */
-    public function isTheme($body=null)
-    {
-        // 테마 환경 설정을 읽어 옵니다.          
-        if ($this->theme = $this->name($body)) {
-
-            // 테마 환경설정파일의 경로
-            $this->_path = $this->path();
-            return $this->themeENV($this->theme, $this->_path);
-        } 
-        
-        // 사이트 테마가 설정되어 있지 않습니다
-        return NULL;
-    }
-
-
-    
-
-
-
-
-
-    /**
-     * 테마의 환경변수를 읽어 옵니다.
-     */
-    public function themeENV($file, $path)
-    {
-        if ($file && $path) {           
-            $path = str_replace("/", File::DS, ROOT.$path.File::DS.$file.File::DS);
-
-            $Conf = Registry::get("CONFIG");
-            $this->_env = $Conf->Drivers['INI']->load("theme", $path);
-            if ($this->_env) {
-                return $this;
-            } else {
-                echo "테마 설정파일을 읽을 수 없습니다.";
-            }            
-        } 
-        
-        //echo "테마가 설정되어 있지 않습니다.<br>";
-        return NULL;     
-    }
-
-
-    /**
-     * 
-     */
-    /*
-    public function loadFile($name)
-    {
-        // $filename = ROOT.File::DS.$this->_path.File::DS.$this->theme.File::DS.$name.".htm";
-        $filename =File::pathImplode([
-            ROOT,
-            $this->_path,
-            $this->theme,
-            $name.".htm"
-        ]);
-        echo $filename;
-        return File::read($filename);
-    }
-    */
-
-
 
     public $theme;
 
@@ -213,6 +73,104 @@ class Theme
     public function getENV()
     {
         return $this->_env;
+    }
+
+
+    /**
+     * 테마 초기화
+     */
+    public function __construct($view=null)
+    {
+        $this->path();
+    }
+
+    public $path;
+    /**
+     * 테마 경로
+     */
+    public function path()
+    {
+        if(isset($this->path)) {
+            return $this->path;
+        } else {
+            $this->path = conf("ENV.path.theme");
+            return $this->path;
+        }
+    }
+
+    /**
+     * 테마 이를을 확인합니다.
+     */
+    public function name($body)
+    {
+        // 커스텀 테마 확인
+        if (isset($body->getData()[self::FRONTMATTER]['theme'])) {
+            // 머리말에 적용할 테마를 직접 지정을 할 경우
+            // 우선 처리합니다.
+            return $body->getData()[self::FRONTMATTER]['theme'];
+        
+        } else {
+            // 기본환경 설정을 적용합니다.
+            return conf("site.theme");
+        }  
+    }
+
+    /**
+     * 테마의 설정을 읽어 옵니다.
+     */
+    public function isTheme($body=null)
+    {
+        // 테마 환경 설정을 읽어 옵니다.          
+        if ($this->theme = $this->name($body)) {
+
+            // 테마 환경설정파일의 경로
+            $this->_path = $this->path();
+            return $this->themeENV($this->theme, $this->_path);
+        } 
+        
+        // 사이트 테마가 설정되어 있지 않습니다
+        return NULL;
+    }
+
+    /**
+     * 테마의 환경변수를 읽어 옵니다.
+     */
+    public function themeENV($file, $path)
+    {
+        if ($file && $path) {           
+            $path = str_replace("/", File::DS, ROOT.$path.File::DS.$file.File::DS);
+
+            $Conf = Registry::get("CONFIG");
+            $this->_env = $Conf->Drivers['INI']->load("theme", $path);
+            if ($this->_env) {
+                return $this;
+            } else {
+                echo "테마 설정파일을 읽을 수 없습니다.";
+            }            
+        } 
+        
+        //echo "테마가 설정되어 있지 않습니다.<br>";
+        return NULL;     
+    }
+
+    /**
+     * 테마를 랜더링 합니다.
+     * 탬플릿 메소드 패턴으로 구현합니다.
+     */
+    public function render($html=null)
+    {
+        if ($this->_env['layout']) { 
+            $layout = \jiny\layout();
+            $code = $this->_env['_content'];
+        } else {
+            // 레이아웃이 없는 경우 바로 출력합니다.
+            $layout = (new Show($this))->progress();
+            $code = "{(content)}";
+        }
+
+        // 본문을 치환합니다.            
+        $layout = str_replace($code, $html->getContent(), $layout); 
+        $html->setContent($layout);
     }
 
     /**
