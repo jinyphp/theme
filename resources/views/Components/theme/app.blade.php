7033eee2049631c58c466be3b418ddf7618363c8
hojinlee <infohojin@gmail.com>
2024-04-06 14:47:53 +0900

    update

--- resources/views/Components/theme/app.blade.php
+++ resources/views/Components/theme/app.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,12 +10,6 @@
                 {{$seo_title}}
             @endif
         </title>
-
-        {{--
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        --}}
-
 
         <link rel="stylesheet" href="https://jinyphp.github.io/css/assets/css/app.css">
         @stack('css')
