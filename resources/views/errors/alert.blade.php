a39a807297e047b879fa8f22d6389b696c3d8331
hojinlee <infohojin@gmail.com>
2024-04-08 17:23:29 +0900

    update

--- resources/views/errors/alert.blade.php
+++ resources/views/errors/alert.blade.php
@@ -1,33 +1,11 @@
-<!doctype html>
-<html lang="ko">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-</head>
-
-<body class="bg-body-tertiary">
-
-    <div class="d-flex justify-content-center align-items-center" style="height: 100vh;">
-        <!-- 화면의 중앙에 정렬된 컨테이너 -->
-        <div class="text-center">
+<x-app>
+    <x-bootstrap>
+        <x-page-center>
             <!-- 메시지를 출력할 요소 -->
             <div class="alert alert-primary" role="alert">
                 {{$message}}
             </div>
             Copyright © 2024 JinyPHP
-        </div>
-    </div>
-
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+        </x-page-center>
+    </x-bootstrap>
+</x-app>
